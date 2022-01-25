402e61d90c0a0ab28f4ca77c79222fbcf38f608c
Anuj2001 <2018034@iiitdmj.ac.in>
2022-01-25 11:14:53 +0530

    Initial Commit

--- myapi/urls.py
+++ myapi/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
-from .views import profile_create_api,profile_list_api,profile_update_api,profile_delete_api
+from .views import profile_api, single_profile_api
 from rest_framework.authtoken.views import obtain_auth_token
 from .custom_token import CustomAuthToken
 
@@ -10,8 +10,6 @@ urlpatterns=[
     #URL used for Token Authentication
     path('gettoken/',CustomAuthToken.as_view()),
 
-    path('api/',profile_list_api.as_view(),name='Get_Profile'),
-    path('api/create/',profile_create_api.as_view(),name='Create_Profile'),
-    path('api/<int:pk>/',profile_update_api.as_view(),name='Update_Profile'),
-    path('api/<int:pk>/delete',profile_delete_api.as_view(),name='Delete_Profile')
+    path('api/',profile_api.as_view()),
+    path('api/<int:pk>/',single_profile_api.as_view()),
 ]
--- myapi/views.py
+++ myapi/views.py
@@ -1,31 +1,22 @@
 from rest_framework import generics
-from rest_framework.response import Response
 from .serializers import profile_serializer
 from .models import Profile
 from .custompermissions import CustomPermissions
 # Create your apis here.
 
 
-# To post some data:
-class profile_create_api(generics.CreateAPIView):
+# To Create or List data:
+class profile_api(generics.ListCreateAPIView):
     queryset = Profile.objects.all()
     serializer_class = profile_serializer
     #permission_classes = [CustomPermissions]
-# To get some data:
-class profile_list_api(generics.ListAPIView):
+
+# To get indivisual data or update or delete some data:
+class single_profile_api(generics.RetrieveUpdateDestroyAPIView):
     queryset = Profile.objects.all()
     serializer_class = profile_serializer
     #permission_classes = [CustomPermissions]
-# To updata a record:
-class profile_update_api(generics.UpdateAPIView):
-    queryset = Profile.objects.all()
-    serializer_class = profile_serializer
-    #permission_classes = [CustomPermissions]
-# To delete a record:
-class profile_delete_api(generics.DestroyAPIView):
-    queryset = Profile.objects.all()
-    serializer_class = profile_serializer
-    #permission_classes = [CustomPermissions]
+
 
 
 
